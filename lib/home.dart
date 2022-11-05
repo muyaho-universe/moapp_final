f188108370db7fc761e1469e44666f425b0b8bb6
muyaho-universe <kimdaesk@gmail.com>
2022-11-05 21:53:00 +0900

    Update dropbox

--- lib/home.dart
+++ lib/home.dart
@@ -30,7 +30,8 @@ class _HomePageState extends State<HomePage> {
   List<Product> products = [];
   static bool isFirst = true;
 
-    List<String> query = <String>['ASC', 'DESC'];
+  static List<String> query = <String>['ASC', 'DESC'];
+  String dropdownValue = query.first;
 
   List<Card> _buildGridCards(BuildContext context) {
     if (isFirst) {
@@ -161,8 +162,30 @@ class _HomePageState extends State<HomePage> {
             return Column(
               children: <Widget>[
                 Container(
-                    color: Colors.grey,
-                  height: 100,
+                  child: DropdownButton<String>(
+                    value: dropdownValue,
+                    icon: const Icon(Icons.arrow_downward),
+                    elevation: 16,
+                    style: const TextStyle(color: Colors.deepPurple),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    onChanged: (String? value) {
+                      // This is called when the user selects an item.
+                      setState(() {
+                        dropdownValue = value!;
+                      });
+                    },
+                    items: query.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+
+                  height: 50,
 
                 ),
                 SizedBox(height: 15,),
