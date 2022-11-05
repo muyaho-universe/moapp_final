ce70bad44c8fef7e8cd27c5840a7b431c70c8256
muyaho-universe <kimdaesk@gmail.com>
2022-11-06 02:07:40 +0900

    detail.dart working

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,10 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'model/product.dart';
+import 'package:intl/intl.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({Key? key, required this.product}) : super(key: key);
   final Product product;
+
   @override
   State<DetailPage> createState() => _DetailPageState();
 }
@@ -12,6 +16,61 @@ class DetailPage extends StatefulWidget {
 class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Detail"),
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: Icon(Icons.create)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 3,
+            child: Image.network(
+              widget.product.image,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Row(
+            children: [
+              Text(widget.product.name),
+              IconButton(onPressed: () {}, icon: Icon(Icons.thumb_up)),
+            ],
+          ),
+          Text(
+            formatter.format(widget.product.price),
+          ),
+          Divider(
+            height: 8,
+            thickness: 1,
+            indent: 8,
+            endIndent: 8,
+            color: Colors.grey,
+          ),
+          Flexible(
+            child: RichText(
+              maxLines: 100,
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(text: widget.product.description),
+                ],
+                style: const TextStyle(
+                  fontSize: 10.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+
+  void format() {}
 }
