0c9622d7c3c39cec8878982953fe11db700ba978
muyaho-universe <kimdaesk@gmail.com>
2022-11-05 23:21:39 +0900

    Update front-end part

--- lib/add.dart
+++ lib/add.dart
@@ -16,6 +16,7 @@ class AddPage extends StatefulWidget {
 class _AddPageState extends State<AddPage> {
   String defaultImage = "logo.png";
   String defaultImamgeURL = "https://handong.edu/site/handong/res/img/logo.png";
+  String pickedImageName = "";
 
   final _productNameController = TextEditingController();
   final _priceController = TextEditingController();
@@ -28,15 +29,19 @@ class _AddPageState extends State<AddPage> {
 
   // late XFile? image;
   bool isLoaded = false;
+  List<String> url = [];
 
   Future getImageFromGalley() async {
     var image =
         await ImagePicker.platform.pickImage(source: ImageSource.gallery);
     setState(() {
       pickedFile = image!;
-      if(pickedFile != null)
-        isLoaded = true;
+      if (pickedFile != null) isLoaded = true;
+      url = pickedFile!.path.split("/");
+      pickedImageName = url.last;
     });
+    print(pickedFile!.path);
+    print(url.last);
   }
 
   @override
@@ -69,26 +74,57 @@ class _AddPageState extends State<AddPage> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 3,
-            child: (isLoaded)
-                ? Image.file(File(pickedFile!.path))
-                : Image.network(
-                    defaultImamgeURL,
-                    fit: BoxFit.cover,
-                  ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconButton(
-                  onPressed: getImageFromGalley, icon: Icon(Icons.camera_alt)),
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 3,
+              child: (isLoaded)
+                  ? Image.file(File(pickedFile!.path))
+                  : Image.network(
+                      defaultImamgeURL,
+                      fit: BoxFit.cover,
+                    ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: getImageFromGalley,
+                  icon: Icon(Icons.camera_alt),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: _productNameController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Product Name',
+              ),
+            ),
+            SizedBox(height: 5),
+            TextField(
+              controller: _priceController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Price',
+              ),
+            ),
+            SizedBox(height: 5),
+            TextField(
+              controller: _descriptionController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Description',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
