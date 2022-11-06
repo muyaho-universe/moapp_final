53f0236449f32ac10a46a8ea642a6707d577bb18
muyaho-universe <kimdaesk@gmail.com>
2022-11-06 12:47:22 +0900

    Update product.dart

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -23,17 +23,24 @@ enum Category {
 
 class Product {
   const Product({
+    required this.liked,
+    required this.creator,
+    required this.uploadTime,
+    required this.editedTime,
     required this.name,
     required this.price,
     required this.image,
     required this.description,
   });
 
-
-
   final String name;
   final int price;
+  final int liked;
   final String image;
   final String description;
 
+  final String creator;
+
+  final String uploadTime;
+  final String editedTime;
 }
