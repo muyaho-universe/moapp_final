960b40a7f22a299028f6dbb481670707fb46d1b9
muyaho-universe <kimdaesk@gmail.com>
2022-11-08 22:55:23 +0900

    Delete useless lines

--- lib/model/product_repo.dart
+++ lib/model/product_repo.dart
@@ -9,27 +9,4 @@ class ProductsRepository {
   static List<Product> loadProducts = [];
   static Map<String, bool> doILike = {};
   static Map<String, bool> doIWish = {};
-
-  // static Future<void> getURL() async {
-  //   var loadProduct = loadProducts.toSet();
-  //   String t = "origin: " + loadProduct.length.toString();
-  //   // print(t);
-  //   for(var proudct in loadProduct){
-  //     bool go = true;
-  //     for(var product in ProductRepo2.loadProducts2){
-  //       if(product.name == proudct.name){
-  //         print(proudct.name);
-  //         go = !go;
-  //       }
-  //     }
-  //      // print(go);
-  //     if(go){
-  //       String image = await FirebaseStorage.instance.ref().child(proudct.image).getDownloadURL();
-  //       ProductRepo2.loadProducts2.add(Product(name: proudct.name, price: proudct.price, image: image, description: proudct.description));
-  //     }
-  //   }
-  //   // ProductRepo2.loadProducts2 = ProductRepo2.loadProducts2.toSet().toList();
-  //   // print(loadProduct.length);
-  // }
-
   }
