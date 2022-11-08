7b130bd2c73963c269500ad8809a73fc78e9399b
muyaho-universe <kimdaesk@gmail.com>
2022-11-09 00:00:53 +0900

    Working on showing the list

--- lib/wish_list.dart
+++ lib/wish_list.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'model/product.dart';
 import 'model/product_repo.dart';
+import 'model/wish_list_model.dart';
 
 class WishListPage extends StatefulWidget {
   const WishListPage({Key? key}) : super(key: key);
@@ -14,6 +16,7 @@ class _WishListPageState extends State<WishListPage> {
   List<Product> wishList = [];
 
   List<Card> _buildListCards(BuildContext context) {
+    final wishProvider = Provider.of<WishProvider>(context);
     List<Product> products = ProductsRepository.loadProducts;
 
     if (products.isEmpty) {
@@ -23,31 +26,39 @@ class _WishListPageState extends State<WishListPage> {
     return products.map((product) {
       var wish = ProductsRepository.doIWish[product.id]!;
 
-      return wish? Card(
-        clipBehavior: Clip.antiAlias,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: 100,
-              height: 130,
-              child: Image.network(
-                product.image,
-                fit: BoxFit.fill,
+      return wish
+          ? Card(
+              clipBehavior: Clip.antiAlias,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 50,
+                    height: 80,
+                    child: Image.network(
+                      product.image,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(product.name),
+                ],
               ),
-            ),
-            SizedBox(width: 20,),
-            Text(product.name),
-          ],
-        ),
-      ): Card();
+            )
+          : Card();
     }).toList();
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Expanded(
-      child: ListView(
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Wish List"),
+      ),
+      body: ListView(
         padding: const EdgeInsets.all(16.0),
         shrinkWrap: true,
         children: _buildListCards(context),
